255e037b7fff08ac68c3d1b2e8a6984abc1dfa0d
Stacy Sauceda <stacysauceda1200@gmail.com>
2025-04-07 23:27:38 -0600

    uidshcnsj

--- app/src/main/java/com/example/carwashdriver_android/MainActivity.java
+++ app/src/main/java/com/example/carwashdriver_android/MainActivity.java
@@ -7,8 +7,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import com.example.carwashdriver_android.R;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,14 +15,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
-        if (savedInstanceState == null) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.fragment_container, new SeleccionLugarFragment())
-                    .commit();
-        }
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
